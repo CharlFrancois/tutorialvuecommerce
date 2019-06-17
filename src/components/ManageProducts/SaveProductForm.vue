a543505b3968defe482138382ab71de21322d33a
STE0043766 <charlesfrancoisjob@gmail.com>
2019-06-17 09:41:37 +0200

    :sparkles: Introducing Add a products

---
+++ src/components/ManageProducts/SaveProductForm.vue
@@ -0,0 +1,33 @@
+<template>
+  <form>
+    <div class="form-group">
+      <label for="productName">Product name</label>
+      <input type="text" v-model="product.name" class="form-control" id="productName" maxlength="32" placeholder="Enter product name">
+    </div>
+    <div class="form-group">
+      <label for="productDescription">Product description <small class="text-muted">(optional)</small></label>
+      <textarea class="form-control" v-model="product.description" id="productDescription" rows="3" maxlength="128" placeholder="Enter description"></textarea>
+    </div>
+    <div class="form-group">
+      <label for="price">Price</label>
+      <input type="number" v-model="product.price" class="form-control" id="price" placeholder="Enter Price" number>
+    </div>
+    <button type="submit" v-on:click.prevent="onsubmit" class="btn btn-primary">Save product</button>
+  </form>
+</template>
+
+<script>
+  export default {
+    name: "SaveProductForm",
+    props:['product'],
+    methods:{
+      onsubmit(){
+        this.$emit('submit', this.product)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
