2af148ff1fd3b958c6e06526c3bb175813a7152a
STE0043766 <charlesfrancoisjob@gmail.com>
2019-06-19 15:44:28 +0200

    :sparkles: Introducing Router

---
+++ src/components/AppNav.vue
@@ -0,0 +1,50 @@
+<template>
+  <nav class="navbar navbar-expand-sm navbar-light navbar-jw">
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#appnav"
+      aria-controls="appnav"
+      aria-expanded="false"
+      aria-label="Toggle Navigation"
+      >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <a class="navbar-brand" href="#">
+      <img src="@/assets/jay.svg" alt="Jayway logo" /> Jaystore
+    </a>
+
+    <div class="collapse navbar-collapse" id="appnav">
+      <div class="nav navbar-nav">
+        <router-link
+          to="home"
+          class="nav-item nav-link"
+          active-class="active"
+          exact
+          >
+          Home
+        </router-link>
+        <router-link
+          to="manage-products"
+          class="nav-item nav-link"
+          active-class="active"
+          exact
+          >
+          Manage Products
+        </router-link>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+  export default {
+    name: "AppNav"
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/ProductCatalog.vue
@@ -0,0 +1,13 @@
+<template>
+  <p>I'm a product catalog!</p>
+</template>
+
+<script>
+  export default {
+    name: "ProductCatalog"
+  }
+</script>
+
+<style scoped>
+
+</style>
