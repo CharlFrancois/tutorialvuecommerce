f0f305df1fcbc2935224bdaa7221b7583bd316e3
STE0043766 <charlesfrancoisjob@gmail.com>
2019-06-19 16:00:35 +0200

    :sparkles: Introducing Router

--- src/main.js
+++ src/main.js
@@ -4,9 +4,12 @@ import Vue from 'vue'
 import './plugins/bootstrap-vue'
 import App from './App.vue'
 import './registerServiceWorker'
+import router from './router'
+import './styles/style.scss'
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
